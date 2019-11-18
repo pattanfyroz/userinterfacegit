e43bd26cb3448a69514370f8c768e1c1a89d4f82
Pattan Fyroz <pattanfyroz.itcom>
2019-11-18 23:09:32 +0530

    first commit

---
+++ app/src/main/java/com/btm/userinterface/MLunch.java
@@ -0,0 +1,14 @@
+package com.btm.userinterface;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MLunch extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mlunch);
+    }
+}
